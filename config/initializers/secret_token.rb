69e43d3ac12b6865bd9f162b761741a105d0235d
Hack Reactor Students <github@hackreactor.com>
2013-06-05 16:05:38 -0700

    added rails server, moved all client stuff to public

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DataNugget::Application.config.secret_token = 'e7d2797ee74c9899752b293c03f5e7a89e5b3a4e7e20266de61a2723c8d3f2d8a29eadbed90cb953d5b552b3f1204147da55aa2c370f56132321c2031e963322'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DataNugget::Application.config.session_store :cookie_store, key: '_dataNugget_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DataNugget::Application.config.session_store :active_record_store
